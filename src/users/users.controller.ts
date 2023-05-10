2ee628ba502644adb3864cbbaed152c7d62fb7cb
steven4program <stevenchang421@gmail.com>
2023-05-10 16:31:16 +0800

    add NotFoundException error message

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,10 +7,12 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { async } from 'rxjs';
 
 @Controller('auth')
 export class UsersController {
@@ -22,8 +24,12 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   @Get()
