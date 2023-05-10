44d4a3c3a2aa64ac0dfb5e572ae8806247d1483c
steven4program <stevenchang421@gmail.com>
2023-05-10 16:20:33 +0800

    implement user CRUD in controller

---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  password: string;
+}
