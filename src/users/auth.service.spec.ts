0c4794bfa04a6783aacaab798ddf206b15d0f3cc
steven4program <stevenchang421@gmail.com>
2023-05-11 17:46:46 +0800

    add password hash & email-in-use check test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,13 +2,16 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
+// import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -31,4 +34,28 @@ describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('5566@test.com', '123456');
+    // expect the user's password is encrypted
+    expect(user.password).not.toEqual('123456');
+    const [salt, hash] = user.password.split('.');
+    // expect the user's password is hashed and salted
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' }] as User[]);
+    await expect(service.signup('6655@test.com', '123456')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 });
+
+// it('throws if signin is called with an unused email', async () => {
+//   await expect(
+//     service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+//   ).rejects.toThrow(NotFoundException);
+// });
